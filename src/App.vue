3eb9cbf625961bfb1fcbab6982fde7b9b1e7655c
RuAleksandr <RuAleksandr07@gmail.com>
2025-01-15 17:42:34 +0500

    Router-L4-end

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
+          <RouterLink :to="{ name: 'EventList' }">Events</RouterLink> |
+          <RouterLink :to="{ name: 'About' }">About</RouterLink>
         </nav>
       </div>
     </header>
---
+++ src/views/event/Register.vue
@@ -0,0 +1,7 @@
+<script setup>
+  const props = defineProps(["event"])
+</script>
+
+<template>
+    <p>Register for the event her</p>
+</template>
